7ba2b0e3f586881cd3ea20c6a95817270ebbd843
Naim <naim@setup.technology>
2021-09-16 14:34:36 +0600

    single product page again deploy

--- assets/scripts.js
+++ assets/scripts.js
@@ -174,5 +174,23 @@ $(document).ready(function () {
 
     });
 
+    $('#main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        infinite: true,
+        adaptiveHeight: true,
+        arrows: true
+    });
+    
+    var video = $('#main-slider .slick-active').find('iframe').get(0).play();
+  
+    $('#main-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+      $('#main-slider .slick-slide').find('video').get(0).pause();
+      var video = $('#main-slider .slick-active').find('video').get(0).play();
+  });
+
 
 });
