12f89da09485d8759ee23320bf244f54dcaa62e3
naimkst <najmulspark@gmail.com>
2022-06-07 22:36:16 +0600

    done

--- assets/scripts.js
+++ assets/scripts.js
@@ -209,7 +209,7 @@ $(document).ready(function () {
     $("#variant-id").val(found.id);
     var priceFind = found.price.toString();
     // $('span.money').html("$" + priceFind.slice(0, -2) + priceFind.slice(0, -2)); 
-    $('#prd-price').html("$" + priceFind.slice(0, -2) + "." + priceFind.slice(-2, 4)); 
+    // $('#prd-price').html("$" + priceFind.slice(0, -2) + "." + priceFind.slice(-2, 4)); 
     
     console.log(`${ priceFind.slice(2) }`);
   });
